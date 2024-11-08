a9fa1a85e31853ebf82b40d4ea78308895b0b829
Serdar-b <serdar-bulun@outlook.com>
2024-11-08 01:51:22 +0100

    Started with hero section

---
+++ app/components/Hero.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="flex justify-center items-center flex-col">
+      <h1>Transform Your Fitness Journey</h1>
+      <p>
+        Track, train, and transform with a personal fitness tracker tailored for
+        you
+      </p>
+    </div>
+  );
+};
+
+export default Hero;
